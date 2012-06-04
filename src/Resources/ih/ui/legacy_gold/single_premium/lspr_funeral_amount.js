3947a60fc4208233a92a6ae65ffca62943cb82fc
Andrew McElroy <Sophrinix@gmail.com>
2012-06-04 13:33:33 -0500

    adding numbers next

--- src/Resources/ih/ui/legacy_gold/single_premium/lspr_funeral_amount.js
+++ src/Resources/ih/ui/legacy_gold/single_premium/lspr_funeral_amount.js
@@ -19,9 +19,150 @@ while (rows.isValidRow()){
 
 //alert('funeral amount');
 
-// single premium due
+// policy face amount
+
 
 //name plate
 
 //2d matrix
 //age prem/1000 total prem
+
+
+
+
+var single_premium_due = Ti.UI.createImageView({
+    image: '/images/single_premium_due.png',
+    width: 240,
+    top: 1
+});
+
+
+var name_plate = Ti.UI.createImageView({
+    image: '/images/name_plate.png',
+    top: 70,
+    width: '95%'
+});
+
+
+Ti.UI.currentWindow.add(single_premium_due);
+Ti.UI.currentWindow.add(name_plate);
+
+var customer_name = Ti.UI.createLabel({
+	top:32,
+	text: Ti.App.Properties.getString('lspr_name') || 'No Name',
+	textAlign: 'left',
+  font: {
+     fontSize: 16,
+     fontWeight: 'bold'
+  },
+  left: 10,
+  color: 'black',
+});
+
+name_plate.add(customer_name);
+
+var volume = Ti.UI.createLabel({
+	top:52,
+	text: 'Funeral Amt: $'+ Ti.App.Properties.getString('lspr_funeral_amount') ,
+	textAlign: 'left',
+  font: {
+     fontSize: 16,
+     fontWeight: 'bold'
+  },
+  left: 10,
+  color: 'black',
+});
+
+name_plate.add(volume);
+
+function getDate(){
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var month = currentTime.getMonth() + 1;
+    var day = currentTime.getDate();
+    var year = currentTime.getFullYear();
+ 
+    return day+"/"+month+"/"+year;
+}   
+
+var date_label = Ti.UI.createLabel({
+	top:32,
+	text: getDate(),
+	textAlign: 'left',
+  font: {
+     fontSize: 16,
+     fontWeight: 'bold'
+  },
+  left: 200,
+  color: 'black',
+});
+
+name_plate.add(date_label);
+
+var issue_age_label = Ti.UI.createLabel({
+	top:52,
+	text: 'Issue Age: '+ Ti.App.Properties.getString('lspr_age') ,
+	textAlign: 'left',
+  font: {
+     fontSize: 16,
+     fontWeight: 'bold'
+  },
+  left: 183,
+  color: 'black',
+});
+
+name_plate.add(issue_age_label);
+
+
+tableview = Titanium.UI.createTableView({
+    top: 135,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    backgroundColor: 'transparent',
+    style: Titanium.UI.iPhone.TableViewStyle.GROUPED
+});
+
+var row = Ti.UI.createTableViewRow({
+    backgroundColor: 'white',
+    height: 30,
+    selectionStyle: Ti.UI.iPhone.TableViewCellSelectionStyle.NONE
+});
+
+var annual = Titanium.UI.createLabel({
+    text: "Annual",
+		textAlign: 'left',
+	  font: {
+	     fontSize: 16,
+	     fontWeight: 'bold'
+	  },
+	  left: 10,
+	  color: 'black',
+});
+
+
+
+var row4 = Ti.UI.createTableViewRow({
+    backgroundColor: 'white',
+    height: 30,
+    selectionStyle: Ti.UI.iPhone.TableViewCellSelectionStyle.NONE
+});
+
+var monthly_direct = Titanium.UI.createLabel({
+    text: "Monthly Direct",
+		textAlign: 'left',
+	  font: {
+	     fontSize: 16,
+	     fontWeight: 'bold'
+	  },
+	  left: 10,
+	  color: 'black',
+});
+
+
+
+Ti.UI.currentWindow.add(tableview);
+
+
+tableview.setData(data);
