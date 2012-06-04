67479ab7680030d561badaa3ace16fc9e1517b85
Andrew McElroy <Sophrinix@gmail.com>
2012-06-04 13:31:18 -0500

    moar progress

--- src/Resources/ih/ui/legacy_gold/single_premium/face_amount.js
+++ src/Resources/ih/ui/legacy_gold/single_premium/face_amount.js
@@ -2,8 +2,11 @@ var data = [];
 
 var db = Titanium.Database.install('../../../../ih.sqlite', 'legacy_single_premium_rates');
 
-//var real_pay = Ti.App.Properties.getString('feii_premium_period').split(' ');
 
+//var rows= db.execute('SELECT * FROM final_expense_rate where issue_age="26" AND plan="Full Benefit" AND  sex="Male" AND  tobacco_status="Tobacco" limit 1; ');
+//var rows = db.execute('SELECT * FROM final_expense_rate where issue_age="'+Ti.App.Properties.getString('feii_issue_age')+'"  AND plan="'+Ti.App.Properties.getString('feii_plan')+'" AND  sex="'+Ti.App.Properties.getString('feii_sex')+'" AND  tobacco_status="'+Ti.App.Properties.getString('feii_tobacco_status')+'" AND pay_period="'+Ti.App.Properties.getString('feii_premium_period')+'" limit 1'); 
+//	alert(Ti.App.Properties.getString('feii_premium_period'));
+//	alert(real_pay[0]);
 
 var rows = db.execute('SELECT * FROM legacy_single_premium_rates limit 1');
 //	alert(rows.rate_per_1000);
@@ -14,4 +17,152 @@ while (rows.isValidRow()){
 			}
 	rows.close();
 
-alert('face amount');
+//alert('funeral amount');
+
+// policy face amount
+
+
+//name plate
+
+//2d matrix
+//age prem/1000 total prem
+
+
+
+
+var policy_face_amount = Ti.UI.createImageView({
+    image: '/images/policy_face_amount.png',
+    width: 240,
+    top: 1
+});
+
+
+var name_plate = Ti.UI.createImageView({
+    image: '/images/name_plate.png',
+    top: 70,
+    width: '95%'
+});
+
+
+Ti.UI.currentWindow.add(policy_face_amount);
+Ti.UI.currentWindow.add(name_plate);
+
+var customer_name = Ti.UI.createLabel({
+	top:32,
+	text: Ti.App.Properties.getString('lspr_name') || 'No Name',
+	textAlign: 'left',
+  font: {
+     fontSize: 16,
+     fontWeight: 'bold'
+  },
+  left: 10,
+  color: 'black',
+});
+
+name_plate.add(customer_name);
+
+var volume = Ti.UI.createLabel({
+	top:52,
+	text: 'Premium: $'+ Ti.App.Properties.getString('lspr_face_amount') ,
+	textAlign: 'left',
+  font: {
+     fontSize: 16,
+     fontWeight: 'bold'
+  },
+  left: 10,
+  color: 'black',
+});
+
+name_plate.add(volume);
+
+function getDate(){
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var month = currentTime.getMonth() + 1;
+    var day = currentTime.getDate();
+    var year = currentTime.getFullYear();
+ 
+    return day+"/"+month+"/"+year;
+}   
+
+var date_label = Ti.UI.createLabel({
+	top:32,
+	text: getDate(),
+	textAlign: 'left',
+  font: {
+     fontSize: 16,
+     fontWeight: 'bold'
+  },
+  left: 200,
+  color: 'black',
+});
+
+name_plate.add(date_label);
+
+var issue_age_label = Ti.UI.createLabel({
+	top:52,
+	text: 'Issue Age: '+ Ti.App.Properties.getString('lspr_age') ,
+	textAlign: 'left',
+  font: {
+     fontSize: 16,
+     fontWeight: 'bold'
+  },
+  left: 183,
+  color: 'black',
+});
+
+name_plate.add(issue_age_label);
+
+
+tableview = Titanium.UI.createTableView({
+    top: 135,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    backgroundColor: 'transparent',
+    style: Titanium.UI.iPhone.TableViewStyle.GROUPED
+});
+
+var row = Ti.UI.createTableViewRow({
+    backgroundColor: 'white',
+    height: 30,
+    selectionStyle: Ti.UI.iPhone.TableViewCellSelectionStyle.NONE
+});
+
+var annual = Titanium.UI.createLabel({
+    text: "Annual",
+		textAlign: 'left',
+	  font: {
+	     fontSize: 16,
+	     fontWeight: 'bold'
+	  },
+	  left: 10,
+	  color: 'black',
+});
+
+
+
+var row4 = Ti.UI.createTableViewRow({
+    backgroundColor: 'white',
+    height: 30,
+    selectionStyle: Ti.UI.iPhone.TableViewCellSelectionStyle.NONE
+});
+
+var monthly_direct = Titanium.UI.createLabel({
+    text: "Monthly Direct",
+		textAlign: 'left',
+	  font: {
+	     fontSize: 16,
+	     fontWeight: 'bold'
+	  },
+	  left: 10,
+	  color: 'black',
+});
+
+
+
+Ti.UI.currentWindow.add(tableview);
+
+
+tableview.setData(data);
--- src/Resources/ih/ui/legacy_gold/single_premium/lspr_funeral_amount.js
+++ src/Resources/ih/ui/legacy_gold/single_premium/lspr_funeral_amount.js
@@ -1,3 +1,12 @@
+var data = [];
+
+var db = Titanium.Database.install('../../../../ih.sqlite', 'legacy_single_premium_rates');
+
+
+//var rows= db.execute('SELECT * FROM final_expense_rate where issue_age="26" AND plan="Full Benefit" AND  sex="Male" AND  tobacco_status="Tobacco" limit 1; ');
+//var rows = db.execute('SELECT * FROM final_expense_rate where issue_age="'+Ti.App.Properties.getString('feii_issue_age')+'"  AND plan="'+Ti.App.Properties.getString('feii_plan')+'" AND  sex="'+Ti.App.Properties.getString('feii_sex')+'" AND  tobacco_status="'+Ti.App.Properties.getString('feii_tobacco_status')+'" AND pay_period="'+Ti.App.Properties.getString('feii_premium_period')+'" limit 1'); 
+//	alert(Ti.App.Properties.getString('feii_premium_period'));
+//	alert(real_pay[0]);
 
 var rows = db.execute('SELECT * FROM legacy_single_premium_rates limit 1');
 //	alert(rows.rate_per_1000);
@@ -8,4 +17,11 @@ while (rows.isValidRow()){
 			}
 	rows.close();
 
-alert('funeral amount');
+//alert('funeral amount');
+
+// single premium due
+
+//name plate
+
+//2d matrix
+//age prem/1000 total prem
