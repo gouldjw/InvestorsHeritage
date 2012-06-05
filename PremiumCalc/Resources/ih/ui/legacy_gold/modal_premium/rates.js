436bff4f2a2685a91d41502e8068bf1030873e54
Andrew McElroy <Sophrinix@gmail.com>
2012-06-05 08:55:30 -0500

    watman

--- PremiumCalc/Resources/ih/ui/legacy_gold/modal_premium/rates.js
+++ PremiumCalc/Resources/ih/ui/legacy_gold/modal_premium/rates.js
@@ -1,6 +1,6 @@
 //alert(Ti.App.Properties.getString('lgm_plan'));
 
-if(Ti.App.Properties.getString('lgm_plan') == 'Simplified Issue'){
+if(Ti.App.Properties.getString('plan') == 'Simplified Issue'){
 		Ti.include('lgm_plan_simple.js');
 }else{
 		Ti.include('lgm_plan_guaranteed.js');
